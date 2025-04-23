179c8f2dc969fe3373e3ce887bf11d42db9ad2a3
l_stevison <lstevison@gmail.com>
2025-04-23 17:56:40 -0500

    update scripts

---
+++ Caloric_Density_Analysis.R
@@ -0,0 +1,57 @@
+##### Load required libraries
+library(pbkrtest)
+library(dplyr)
+library(reshape2)
+library(stringi)
+library(stringr)
+library(ggplot2)
+library(emmeans)
+library(lme4)
+library(car)
+library(ggthemes)
+library(doBy)
+library(MASS)
+library(cowplot)
+library(lmerTest)
+
+
+##### RUN ANALYSIS
+##
+### Load custom functions
+## Assign stars based on level of significance
+eval.significance <- function(input) {
+  ifelse(input$p.value < 0.001, "***",
+         ifelse(input$p.value < 0.01, "**",
+                ifelse(input$p.value < 0.05, "*", "")))
+}
+
+### Define haplotype groups
+source("scripts/haplotype-groups.R")
+
+# Read in cleaned up anonymized phenotype data
+data=read.csv(file="rawdata/phenotype-data-anon.csv",stringsAsFactors = F)
+
+# Cleanup backcross form data (F1 cross info)
+source("scripts/backcross-cleanup.R")
+
+# Merge phenotype data with backcross/treatment data
+source("scripts/merge-data.R")
+
+#save cleaned merged datasets
+write.csv(merged,file="output/phenotyping_data_cleaned.csv",row.names = F,quote = F)
+write.csv(by_vialday,file="output/data_cleaned_by_vialday.csv",row.names = F,quote = F)
+
+# Calculate mean recombination by treatment and strain
+source("scripts/recombination-rate.R")
+
+# Calculate fecundity by vial by day and avg fecundity/mom/day
+source("scripts/fecundity.R")
+
+# Combine all data into one summary data.frame
+summary <- merge(recomb_summary, fecund_summary, by = c("Treatment", "PaternalStock"))
+
+# Calculate haplotype bias
+source("scripts/haplotype-bias.R")
+
+# Calculate Crossover Interference
+source("scripts/COI.R")
---
+++ scripts/fecundity.R
@@ -0,0 +1,58 @@
+fecund <- by_vialday[, c("Treatment", "PaternalStock", "vial_num", "vial_letter", "num_offspring", "num_moms")]
+
+## Remove rows with less than 10 offspring
+#### fecund <- fecund[fecund$num_offspring >= 10,]
+
+## Calculate per vial per letter fecundity
+fecund$fecundity <- fecund$num_offspring/fecund$num_moms
+
+fecund_by_vial <- aggregate(fecundity ~ vial_num+vial_letter, data = fecund, sum)
+fecund_by_vial <- merge(fecund_by_vial, backcross, by.x = "vial_num", by.y = "CrossID")
+
+fecund_summary <- aggregate(fecundity ~ Treatment + PaternalStock, data = fecund_by_vial, mean)
+
+#make day and vial number a factor
+fecund_by_vial$vial_letter=as.factor(fecund_by_vial$vial_letter)
+fecund_by_vial$vial_num=as.factor(fecund_by_vial$vial_num)
+
+## Poisson
+#fit_fec <- glmer(fecundity ~ (1|MaternalVial)+Treatment*PaternalStock*vial_letter, data = fecund_by_vial, family = "poisson")
+#fecundity_stats <- Anova(fit_fec, test = "Chisq")
+
+##Negative Binomial
+fit_fec <- glmer.nb(fecundity ~ (1|MaternalVial)+Treatment*PaternalStock*vial_letter, data = fecund_by_vial)
+fecundity_stats <- Anova(fit_fec)
+fecundity_stats
+
+write.csv(fecundity_stats, "output/fecundity-stats_anova.csv")
+
+## Post hoc test for fecundity
+fit_contrast_fec <- emmeans::emmeans(fit_fec, specs = pairwise ~ Treatment, by=c("PaternalStock","age"), mode="kenward-roger")
+fit_contr_fec <- contrast(fit_contrast_fec, method="pairwise")
+
+pheno_contr_fec <- as.data.frame(summary(fit_contr_fec))
+pheno_contr_fec
+
+write.csv(pheno_contr_fec,"output/fecundity_posthoc.csv")
+
+
+## Fecundity figure
+fecund_figure <- ggplot(fecund_by_vial, aes(x=Treatment, y=fecundity, col=PaternalStock)) +
+  xlab("Caloric Density") + ylab("# progeny per mom") + ggtitle("Fecundity") +
+  ylim(0, 80) +
+  theme(axis.text.x = element_text(angle = 0)) +
+  geom_boxplot() + scale_color_discrete(name = "Treatment")
+
+fecund_figure
+ggsave("images/fecundity-figure.png")
+
+## Fecundity figure by day
+fecund_figure <- ggplot(fecund_by_vial, aes(x=Treatment, y=fecundity, col=PaternalStock)) + facet_wrap(~vial_letter) +
+  xlab("Caloric Density") + ylab("# progeny per mom") + ggtitle("Fecundity") +
+  ylim(0, 80) +
+  theme(axis.text.x = element_text(angle = 0)) +
+  geom_boxplot() + scale_color_discrete(name = "Treatment")
+
+fecund_figure
+ggsave("images/fecundity-figure-byDay.png")
+
